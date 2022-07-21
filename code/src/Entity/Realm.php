769bb43052a3e8d63d5b387c9ebbb7d9578068db
Quentin ADADAIN <quentin.adadain@gmail.com>
2022-07-21 02:04:49 +0200

    Add entities for character whit other entities

--- code/src/Entity/Realm.php
+++ code/src/Entity/Realm.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RealmRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RealmRepository::class)]
@@ -10,17 +12,25 @@ class Realm
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column()]
-    private ?int $id = null;
+    #[ORM\Column(nullable: false)]
+    private int $id;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private string $name;
 
     #[ORM\ManyToOne(inversedBy: 'realms')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Region $region = null;
+    private Region $region;
 
-    public function getId(): ?int
+    #[ORM\OneToMany(mappedBy: 'realm', targetEntity: Character::class)]
+    private Collection $characters;
+
+    public function __construct()
+    {
+        $this->characters = new ArrayCollection();
+    }
+
+    public function getId(): int
     {
         return $this->id;
     }
@@ -37,7 +47,7 @@ class Realm
         return $this;
     }
 
-    public function getRegion(): ?Region
+    public function getRegion(): Region
     {
         return $this->region;
     }
@@ -45,6 +55,36 @@ class Realm
     public function setRegion(?Region $region): self
     {
         $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Character>
+     */
+    public function getCharacters(): Collection
+    {
+        return $this->characters;
+    }
+
+    public function addCharacter(Character $character): self
+    {
+        if (!$this->characters->contains($character)) {
+            $this->characters[] = $character;
+            $character->setRealm($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCharacter(Character $character): self
+    {
+        if ($this->characters->removeElement($character)) {
+            // set the owning side to null (unless already changed)
+            if ($character->getRealm() === $this) {
+                $character->setRealm(null);
+            }
+        }
 
         return $this;
     }
